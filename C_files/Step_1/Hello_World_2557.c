7aff52d969fce792d4e036a141eccd2496adcff5
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-01 21:30:31 +0900

    No.2557 files

---
+++ C_files/Step_1/Hello_World_2557.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("æ»≥Á«œººø‰ π›∞©Ω¿¥œ¥Ÿ");
+    int a = 10;
+    int b = 20;
+    int c;
+    c = a + b;
+    printf("a + b = %d\n", c);
+    for (int i = 1; i < 10; i++) {
+        printf("C = %d\n", c - i);
+    }
+
+    return 0;
+}
