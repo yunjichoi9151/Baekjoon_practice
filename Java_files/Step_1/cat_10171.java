f78c8c2e6eb2039e93aa7895ceb823138a3901f0
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-02 15:59:44 +0900

    No.10171 files

---
+++ Java_files/Step_1/cat_10171.java
@@ -0,0 +1,10 @@
+package Step_1;
+
+public class cat_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
