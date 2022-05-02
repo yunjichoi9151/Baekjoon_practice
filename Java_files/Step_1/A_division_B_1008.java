35c4295a00fd5a32dec942b824178e40c1de71bf
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-03 01:59:02 +0900

    No.1008 files

---
+++ Java_files/Step_1/A_division_B_1008.java
@@ -0,0 +1,11 @@
+package Step_1;
+import java.util.Scanner;
+
+public class A_division_B_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a / b);
+    }
+}
