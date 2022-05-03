83638be3de33f59279c56e80ad19ffe58a0ab7e2
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-04 01:50:58 +0900

    No.18108 files

---
+++ Java_files/Step_1/Thai_Year_Conversion_18108.java
@@ -0,0 +1,10 @@
+package Step_1;
+import java.util.Scanner;
+
+public class Thai_Year_Conversion_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a - 543);
+    }
+}
