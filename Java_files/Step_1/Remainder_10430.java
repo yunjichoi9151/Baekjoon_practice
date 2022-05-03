7114fdaaa7851f3621ed4ebf990a48ac0bf065cd
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-04 02:00:21 +0900

    No.10430 files

---
+++ Java_files/Step_1/Remainder_10430.java
@@ -0,0 +1,15 @@
+package Step_1;
+import java.util.Scanner;
+
+public class Remainder_10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.print(((a % c) * (b % c)) % c);
+    }
+}
