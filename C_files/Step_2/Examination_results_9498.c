82c1a7c286b624d0abeb18e4b75189f44182ddd9
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-05 00:41:56 +0900

    Modify files

--- C_files/Step_2/Examination_results_9498.c
+++ C_files/Step_2/Examination_results_9498.c
@@ -6,13 +6,13 @@ int main()
 
     scanf("%d", &a);
 
-    if (a <= 100 & a >= 90)
+    if (a <= 100 && a >= 90)
         printf("A");
-    else if (a <= 89 & a >= 80)
+    else if (a <= 89 && a >= 80)
         printf("B");
-    else if (a <= 79 & a >= 70)
+    else if (a <= 79 && a >= 70)
         printf("C");
-    else if (a <= 69 & a >= 60)
+    else if (a <= 69 && a >= 60)
         printf("D");
     else
         printf("F");
