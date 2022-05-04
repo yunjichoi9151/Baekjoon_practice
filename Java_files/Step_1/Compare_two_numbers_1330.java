51b466237d78950add007a70503d131edabcea4d
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-05 00:03:48 +0900

    No.1330 files

---
+++ Java_files/Step_1/Compare_two_numbers_1330.java
@@ -0,0 +1,16 @@
+package Step_1;
+import java.util.Scanner;
+
+public class Compare_two_numbers_1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a < b)
+            System.out.print("<");
+        else if(a > b)
+            System.out.print(">");
+        else if(a == b)
+            System.out.print("==");
+    }
+}
