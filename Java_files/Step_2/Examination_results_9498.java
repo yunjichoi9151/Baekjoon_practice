4591415e2eab9d48c85830e0741956e596352138
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-05 00:30:23 +0900

    No.9498 files

---
+++ Java_files/Step_2/Examination_results_9498.java
@@ -0,0 +1,19 @@
+package Step_2;
+import java.util.Scanner;
+
+public class Examination_results_9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a <= 100 & a >= 90)
+            System.out.print("A");
+        else if(a <= 89 & a >= 80)
+            System.out.print("B");
+        else if(a <= 79 & a >= 70)
+            System.out.print("C");
+        else if(a <= 69 & a >= 60)
+            System.out.print("D");
+        else
+            System.out.print("F");
+    }
+}
