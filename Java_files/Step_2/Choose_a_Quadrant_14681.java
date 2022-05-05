90873768baa491f68e9e9aa3e91347e907ed10ae
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-06 01:51:30 +0900

    No.14681 files

---
+++ Java_files/Step_2/Choose_a_Quadrant_14681.java
@@ -0,0 +1,18 @@
+package Step_2;
+import java.util.Scanner;
+
+public class Choose_a_Quadrant_14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a > 0 && b > 0)
+            System.out.print("1");
+        else if(a < 0 && b > 0)
+            System.out.print("2");
+        else if(a < 0 && b < 0)
+            System.out.print("3");
+        else if(a > 0 && b < 0)
+            System.out.print("4");
+    }
+}
