b0f570307c5c1a86422ef5999b2e62a31e179dd7
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-09 02:06:45 +0900

    No.10951 files

---
+++ Java_files/Step_3/A_plus_B_4_10951.java
@@ -0,0 +1,13 @@
+package Step_3;
+import java.util.Scanner;
+
+public class A_plus_B_4_10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
