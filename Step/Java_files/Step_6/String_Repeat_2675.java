99ea0366b988b74969403b034528abd130de0cde
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-14 04:09:49 +0900

    No.2675 files

---
+++ Step/Java_files/Step_6/String_Repeat_2675.java
@@ -0,0 +1,19 @@
+package Step_6;
+import java.util.Scanner;
+
+public class String_Repeat_2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int r = sc.nextInt();
+            String s = sc.next();
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < r; k++) {
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
