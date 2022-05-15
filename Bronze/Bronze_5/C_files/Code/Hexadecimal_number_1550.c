c835960abf2065785d13e3d335a1dab02953af46
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-16 03:49:34 +0900

    No.1550 files

---
+++ Bronze/Bronze_5/C_files/Code/Hexadecimal_number_1550.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    scanf("%x",&a);
+
+    printf("%d",a);
+    
+    return 0;
+}
---
+++ Bronze/Bronze_5/Java_files/Code/Hexadecimal_number_1550.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Hexadecimal_number_1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(16);
+        System.out.println(a);
+    }
+}
