1ffe6c2a15caf269365dcc25d3f1d3d379fe325f
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-16 02:56:38 +0900

    No.1271 files

---
+++ Bronze/Bronze_5/Java_files/Code/Man_of_Great_Wealth_1271.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Man_of_Great_Wealth_1271 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
