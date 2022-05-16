5ad1cf4da760d829c85b16f3e31369b7b5af9b6b
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-17 01:43:21 +0900

    No.2558 files

---
+++ Bronze/Bronze_5/Java_files/Code/A_plus_B_2_2558.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class A_plus_B_2_2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
