d4e2751c33c4964ec3935bebf5d08499279faf7d
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-19 03:05:55 +0900

    No.5339 files

---
+++ Bronze/Bronze_5/Java_files/Code/Call_center_5339.java
@@ -0,0 +1,14 @@
+public class Call_center_5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
