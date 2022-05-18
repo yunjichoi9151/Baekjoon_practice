588c7ecbc759cbd649bc2f2d3f692a2f7a409974
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-19 03:06:33 +0900

    No.5522 files

---
+++ Bronze/Bronze_5/Java_files/Code/Card_Game_5522.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Card_Game_5522 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = sc.nextInt();
+            sum += a;
+        }
+        System.out.println(sum);
+    }
+}
