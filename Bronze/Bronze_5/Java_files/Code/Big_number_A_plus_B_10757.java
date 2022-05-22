f878e381f874ea573fb3b3770fc15d3085eba0db
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-23 00:52:12 +0900

    No.10757 files

---
+++ Bronze/Bronze_5/Java_files/Code/Big_number_A_plus_B_10757.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Big_number_A_plus_B_10757 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
