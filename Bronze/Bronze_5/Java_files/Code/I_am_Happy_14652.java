ded7d76329db6fc7db764f6df555a620a49c50a0
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-24 01:28:12 +0900

    No.14652 files

---
+++ Bronze/Bronze_5/Java_files/Code/I_am_Happy_14652.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class I_am_Happy_14652 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(k / m + " " + k % m);
+    }
+}
