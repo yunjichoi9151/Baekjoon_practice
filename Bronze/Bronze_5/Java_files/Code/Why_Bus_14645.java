368e0f5cf6f550b7bc99ee909d2aad77609c44ec
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-24 01:27:29 +0900

    No.14645 files

---
+++ Bronze/Bronze_5/Java_files/Code/Why_Bus_14645.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Why_Bus_14645 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
