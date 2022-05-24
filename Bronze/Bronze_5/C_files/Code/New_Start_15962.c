643baf50bcae0d9c3926b2afd0d7115081246934
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-25 03:00:50 +0900

    No.15962 files

---
+++ Bronze/Bronze_5/C_files/Code/New_Start_15962.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("ÆÄÀÌÆÃ!!");
+
+    return 0;
+}
---
+++ Bronze/Bronze_5/Java_files/Code/New_Start_15962.java
@@ -0,0 +1,5 @@
+public class New_Start_15962 {
+    public static void main(String[] args) {
+        System.out.println("파이팅!!");
+    }
+}
