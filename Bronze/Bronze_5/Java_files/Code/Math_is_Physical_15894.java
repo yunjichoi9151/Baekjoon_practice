c74eac660d2b46706c6eb448784e8556f8a32d1f
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-25 03:00:20 +0900

    No.15894 files

---
+++ Bronze/Bronze_5/Java_files/Code/Math_is_Physical_15894.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Math_is_Physical_15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = new BigInteger(String.valueOf(sc.nextInt()));
+        System.out.println(n.multiply(BigInteger.valueOf(4)));
+    }
+}
