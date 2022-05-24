44681e8adecfb7e60940837e2e6c28e3fa88dc49
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-25 02:59:44 +0900

    No.15740 files

---
+++ Bronze/Bronze_5/Java_files/Code/A_plus_B_9_15740.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class A_plus_B_9_15740 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input =br.readLine().split(" ");
+        BigInteger a = new BigInteger(input[0]);
+        BigInteger b = new BigInteger(input[1]);
+        System.out.print((a.add(b)).toString());
+    }
+}
