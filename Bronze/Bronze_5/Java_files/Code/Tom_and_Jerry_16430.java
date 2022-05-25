3b7324b26d798e24caa7cdeb3c8bbeaee11de45f
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-26 03:47:41 +0900

    No.16430 files

---
+++ Bronze/Bronze_5/Java_files/Code/Tom_and_Jerry_16430.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Tom_and_Jerry_16430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((b - a) + " " + b);
+    }
+}
