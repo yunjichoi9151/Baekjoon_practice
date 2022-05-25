ba23be69357dd211d66e89eabacac0a0889e05d7
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-26 03:47:14 +0900

    No.16394 files

---
+++ Bronze/Bronze_5/C_files/Code/Hongik_University_16394.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int var;
+
+    scanf("%d", &var);
+
+    printf("%d", var - 1946);
+
+    return 0;
+}
---
+++ Bronze/Bronze_5/Java_files/Code/Hongik_University_16394.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Hongik_University_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int var = sc.nextInt();
+        System.out.println(var - 1946);
+    }
+}
