30f3e413b4e2827b937f369d827814f331eff477
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-26 03:46:05 +0900

    No.15964 files

---
+++ Bronze/Bronze_5/Java_files/Code/Strange_Symbol_15964.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Strange_Symbol_15964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println((a + b) * (a - b));
+    }
+}
