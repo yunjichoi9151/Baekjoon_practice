5d5c9507c078816ae42ae2fb35774aa46d682f2c
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-27 01:18:00 +0900

    No.17256 files

---
+++ Bronze/Bronze_5/Java_files/Code/More_Sweet_17256.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class More_Sweet_17256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ax = sc.nextInt();
+        int ay = sc.nextInt();
+        int az = sc.nextInt();
+        int cx = sc.nextInt();
+        int cy = sc.nextInt();
+        int cz = sc.nextInt();
+        
+        System.out.println((cx - az) + " " + (cy / ay) + " " + (cz - ax));
+    }
+}
