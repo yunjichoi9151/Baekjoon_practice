97a888598b4eeec4a6e9ecd6baa23bfd93c50f55
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-28 20:39:30 +0900

    No.20254 files

---
+++ Bronze/Bronze_5/Java_files/Code/Site_Score_20254.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Site_Score_20254 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int u_r = sc.nextInt();
+        int t_r = sc.nextInt();
+        int u_o = sc.nextInt();
+        int t_o = sc.nextInt();
+        System.out.println(56 * u_r + 24 * t_r + 14 * u_o + 6 * t_o);
+    }
+}
---
+++ Bronze/Bronze_5/Python_files/Code/Site_Score_20254.py
@@ -0,0 +1,2 @@
+u_r, t_r, u_o, t_o = map(int, input().split())
+print(56 * u_r + 24 * t_r + 14 * u_o + 6 * t_o)
