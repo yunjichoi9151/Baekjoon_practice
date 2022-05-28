9160ea31654b6d0e805506abfd209b2fb9c92fae
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-29 03:47:30 +0900

    No.23234 files

---
+++ Bronze/Bronze_5/C_files/Code/The_World_Responds_23234.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The world says hello!");
+
+    return 0;
+}
---
+++ Bronze/Bronze_5/Java_files/Code/The_World_Responds_23234.java
@@ -0,0 +1,5 @@
+public class The_World_Responds_23234 {
+    public static void main(String[] args) {
+        System.out.println("The world says hello!");
+    }
+}
