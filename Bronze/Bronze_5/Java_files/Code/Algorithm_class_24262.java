7122323dc040afab0efce1b8b78c9682b28e0e9e
yunjichoi9151 <cyjcode99@gmail.com>
2022-05-30 00:21:30 +0900

    No.24262 files

---
+++ Bronze/Bronze_5/Java_files/Code/Algorithm_class_24262.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Algorithm_class_24262 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
