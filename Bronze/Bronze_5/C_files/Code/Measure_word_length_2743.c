d3bf29b95b6c541cbec785f98ae80bd029401aac
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-06 00:05:54 +0900

    No.2743 files

---
+++ Bronze/Bronze_5/C_files/Code/Measure_word_length_2743.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[101];
+
+    scanf("%s", &s);
+
+    printf("%d", strlen(s));
+}
---
+++ Bronze/Bronze_5/Java_files/Code/Measure_word_length_2743.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Measure_word_length_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.length());
+    }
+}
