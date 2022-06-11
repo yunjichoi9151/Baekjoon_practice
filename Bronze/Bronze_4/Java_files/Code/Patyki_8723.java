917b64e14f3b75f9ef6bd1b21dfcb7b62fbeacfe
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-12 00:56:07 +0900

    No.8723 files

---
+++ Bronze/Bronze_4/Java_files/Code/Patyki_8723.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Patyki_8723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b && b == c && a == c)
+        System.out.println("2");
+        else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b)
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
