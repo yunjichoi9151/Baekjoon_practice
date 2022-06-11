d398feda9c891ca4f803cb53587b61fec5a738e6
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-11 13:35:23 +0900

    No.13985 files

---
+++ Bronze/Bronze_4/Java_files/Code/Equality_13985.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Equality_13985 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String d = sc.next();
+        int b = sc.nextInt();
+        String e = sc.next();
+        int c = sc.nextInt();
+        System.out.println((a + b == c) ? "YES" : "NO");
+    }
+}
