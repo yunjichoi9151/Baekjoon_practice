1a3a5cf313b90f60dd3b2d5de481642c76bc02f0
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-16 01:32:02 +0900

    No.14264 files

---
+++ Bronze/Bronze_4/Java_files/Code/Regular_Hexagon_and_Triangle_14264.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Regular_Hexagon_and_Triangle_14264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double l = sc.nextDouble();
+        System.out.printf("%.10f", l * l * Math.sqrt(3) / 4);
+    }
+}
