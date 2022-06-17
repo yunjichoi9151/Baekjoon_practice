c1622de4441349d56db7af6dd2b241d5ba187ed1
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-17 13:30:52 +0900

    No.14581 files

---
+++ Bronze/Bronze_4/Java_files/Code/Hongjun_with_fan_14581.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hongjun_with_fan_14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + s + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
