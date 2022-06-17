05cf55289f4665e2f4ee8d1b8e66d723bd3a0ca6
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-18 01:43:43 +0900

    No.14935 files

---
+++ Bronze/Bronze_4/Java_files/Code/FA_14935.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FA_14935 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println("FA");
+    }
+}
