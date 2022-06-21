e684fda86f8e822404de38f98e9a376f2188b7ff
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-21 17:16:35 +0900

    No.15610 files add

---
+++ Bronze/Bronze_4/Java_files/Code/Abbey_Courtyard_15610.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Abbey_Courtyard_15610 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("%.7f", Math.sqrt(a) * 4);
+    }
+}
