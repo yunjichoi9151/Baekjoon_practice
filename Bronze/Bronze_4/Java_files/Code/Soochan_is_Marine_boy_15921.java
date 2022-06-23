f9e52dac2f81c22bdd806cb9a0cb1b8b50e8746f
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-23 13:59:49 +0900

    No.15921 files add

---
+++ Bronze/Bronze_4/Java_files/Code/Soochan_is_Marine_boy_15921.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Soochan_is_Marine_boy_15921 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((n == 0) ? "divide by zero" : "1.00");
+    }
+}
