f880f7eba3c907c89584ee4db3aa355caa381673
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-27 14:08:21 +0900

    No.16486 files

---
+++ Bronze/Bronze_4/Java_files/Code/Playground_16486.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Playground_16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double d1 = sc.nextDouble(), d2 = sc.nextDouble();
+        System.out.printf("%.6f", d1 * 2 + d2 * 2 * 3.141592);
+    }
+}
