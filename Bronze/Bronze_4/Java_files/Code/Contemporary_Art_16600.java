41683c41a87e6907f8f4ae95b90bb624a1fc47f9
yunjichoi9151 <cyjcode99@gmail.com>
2022-06-27 14:08:48 +0900

    No.16600 files

---
+++ Bronze/Bronze_4/Java_files/Code/Contemporary_Art_16600.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Contemporary_Art_16600 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(Math.sqrt(a) * 4);
+    }
+}
