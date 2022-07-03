f91849aa20828e2fdfc96cd6c5d4f3a6631b0578
yunjichoi9151 <cyjcode99@gmail.com>
2022-07-03 13:47:52 +0900

    No.20353 files

---
+++ Bronze/Bronze_4/C_files/Code/Atrium_20353.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a;
+
+    scanf("%lf", &a);
+
+    printf("%lf", sqrt(a) * 4);
+
+    return 0;
+}
---
+++ Bronze/Bronze_4/Java_files/Code/Atrium_20353.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Atrium_20353 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(Math.sqrt(a) * 4);
+    }
+}
