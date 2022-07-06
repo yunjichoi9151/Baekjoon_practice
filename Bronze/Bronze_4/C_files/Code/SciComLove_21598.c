2f6a42ae0eb2d1704db3ee927e1377a3640833d0
yunjichoi9151 <cyjcode99@gmail.com>
2022-07-06 23:44:25 +0900

    No.21598 files

---
+++ Bronze/Bronze_4/C_files/Code/SciComLove_21598.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        printf("SciComLove\n");
+
+    return 0;
+}
---
+++ Bronze/Bronze_4/Java_files/Code/SciComLove_21598.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SciComLove_21598 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i ++)
+            System.out.println("SciComLove");
+    }
+}
