d7b9bb0c810c58f023a29acabd51ad89facdf2be
yunjichoi9151 <cyjcode99@gmail.com>
2022-07-08 00:01:55 +0900

    No.23235 files add

---
+++ Bronze/Bronze_4/Java_files/Code/The_Fastest_Sorting_Algorithm_In_The_World_23235.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class The_Fastest_Sorting_Algorithm_In_The_World_23235 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String array = sc.nextLine();
+		for (int i = 1; !array.equals("0"); i++) {
+			System.out.println("Case " + i + ": Sorting... done!");
+			array = sc.nextLine();
+		}
+    }
+}
