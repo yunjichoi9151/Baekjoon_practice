877c558ec3c9d6abaa87c2bcc3baec2689e93c6c
yunjichoi9151 <cyjcode99@gmail.com>
2022-07-12 09:50:43 +0900

    No.24365 files

---
+++ Bronze/Bronze_4/Java_files/Code/Queen_bee_Maya_24365.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Queen_bee_Maya_24365 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        System.out.println(c - a);
+    }
+}
